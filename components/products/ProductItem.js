230af76bdb62db4b78e4353e6de6fdbfbac8c4c4
Cvijo <matija@vipdata.hr>
2019-12-28 17:16:12 +0100

    added ProductItem design

--- components/products/ProductItem.js
+++ components/products/ProductItem.js
@@ -2,12 +2,76 @@ import React from "react";
 import { PropTypes } from "prop-types";
 
 const ProductItem = ({ link }) => {
+  const imgUrl = link.imageUrl;
   return (
-    <li>
-        <a href={link.url}>
-          {link.idLink} - {link.title}
+    <div>
+      <div className='product-card' key={link.idLink}>
+        <a onClick={() => this.handleDeleteLink(link.idLink)}>
+          <i className='far fa-fw fa-trash-alt product-delete' />
         </a>
-    </li>
+        <div className='product-image'>
+          <a href={link.url} target='_blank' rel='noopener noreferrer'>
+            <img alt='product' src={imgUrl || "/img/image-placeholder.png"} />
+          </a>
+        </div>
+        <div className='product-info'>
+          <div>
+            <a href={link.url} target='_blank' rel='noopener noreferrer'>
+              <strong className='product-link'>
+                {link.storeName || "- not scraped -"}
+              </strong>
+            </a>
+          </div>
+          <div>
+            Price: {link.priceMin} - {link.priceMax} {link.priceMinCurrency}
+          </div>
+          <div>Rating: {link.rating}</div>
+          <div>Sales: {link.salesCount}</div>
+        </div>
+      </div>
+      {/* <a href={link.url}>
+          {link.idLink} - {link.title}
+        </a> */}
+      <style jsx>{`
+        .product-card {
+          flex: 1 16%;
+          margin: 10px;
+          max-width: 200px;
+          min-width: 200px;
+          border: solid 1px #ddd;
+          padding: 10px;
+          position: relative;
+        }
+
+        .product-image img {
+          max-width: 100%;
+          padding: 5px;
+          margin-bottom: 10px;
+        }
+
+        .product-info {
+          margin-top: auto;
+          font-size: 14px;
+        }
+
+        .product-link {
+          color: black;
+        }
+
+        .product-delete {
+          position: absolute;
+          right: 10px;
+          bottom: 12px;
+          cursor: pointer;
+        }
+
+        @media (max-width: 920px) {
+          .product-card {
+            flex: 1 21%;
+          }
+        }
+      `}</style>
+    </div>
   );
 };
 
--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,42 +1,44 @@
 import { useRouter } from "next/router";
-import Layout from '../../components/Layout';
-import { PropTypes } from 'prop-types';
-import ProductItem from './../../components/products/ProductItem';
-import fetch from 'isomorphic-unfetch';
-
+import Layout from "../../components/Layout";
+import { PropTypes } from "prop-types";
+import ProductItem from "./../../components/products/ProductItem";
+import fetch from "isomorphic-unfetch";
 
 const allProductLinksUrl =
   "https://offers.xiaomipedia.com/api/v1/links?api_key=ali7w4fhopq34ifn93784fg";
 
-
-
-
-
-const ProductDetail = ({links}) => {
+const ProductDetail = ({ links }) => {
   const router = useRouter();
   return (
     <Layout>
-        {links && links.map(link => {
-            return(
-                <ProductItem key={link.idLink} link={link}/>
-            );
-        })}
+      <div className='products'>
+        {links &&
+          links.map(link => {
+            return <ProductItem key={link.idLink} link={link} />;
+          })}
+        <style jsx>{`
+          .products {
+            display: flex;
+            flex-wrap: wrap;
+          }
+        `}</style>
+      </div>
     </Layout>
   );
 };
 
-ProductDetail.getInitialProps = async (context) => {
-    const { id } = context.query;
-    const res = await fetch(`${allProductLinksUrl}&product_id=${id}`);
-    const data = await res.json();
-    console.log("fetched links data count: " + data.length);
-    return {
-      links: data
-    };
+ProductDetail.getInitialProps = async context => {
+  const { id } = context.query;
+  const res = await fetch(`${allProductLinksUrl}&product_id=${id}`);
+  const data = await res.json();
+  console.log("fetched links data count: " + data.length);
+  return {
+    links: data
   };
+};
 
 ProductDetail.propTypes = {
-    links: PropTypes.arrayOf(PropTypes.object).isRequired,
-}
-  
+  links: PropTypes.arrayOf(PropTypes.object).isRequired
+};
+
 export default ProductDetail;
